8e4ef0ae2f76cde044d11f3453d190a4df0319d1
Cesar Giupponi <cesar.giupponi@gmail.com>
2025-11-27 09:43:31 -0300

    feat: add the project

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Cesar Giupponi on 27/11/25.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
