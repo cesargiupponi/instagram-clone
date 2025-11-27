850b6d53cb14cdcd63f4fc20eed84d997108cae2
Cesar Giupponi <cesar.giupponi@gmail.com>
2025-11-27 16:08:31 -0300

    chore: add images

---
+++ InstagramClone/Core/Components/UserStatView.swift
@@ -0,0 +1,18 @@
+//
+//  UserStatView.swift
+//  InstagramClone
+//
+//  Created by Cesar Giupponi on 27/11/25.
+//
+
+import SwiftUI
+
+struct UserStatView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserStatView()
+}
